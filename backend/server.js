85c676e98703efb5896d87adc3e69f85297f17c5
Keshiv Tandon <keshivshaan@gmail.com>
2024-10-07 17:35:32 -0700

    bcrypt turned into bcryptjs

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { body, validationResult } = require('express-validator');
 const multer = require('multer');
